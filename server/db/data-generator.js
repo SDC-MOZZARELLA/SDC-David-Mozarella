78ef25ab17c3453e1498ce8c996d311976673046
David Fuentes <D.gabefuentes@gmail.com>
2019-11-02 11:46:04 -0700

    takes out superfluous code and refactors role and line 5

--- server/db/data-generator.js
+++ server/db/data-generator.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const faker = require('faker');
-const writer = require('csv-write-stream');
-const csv = writer();
 
 const generated = 10000000;
-let movieIdCounter = 1;
+let movieIdCounter = 0;
 let castIdCounter = -1;
 
 const movieCSV = fs.createWriteStream(
@@ -13,6 +11,10 @@ const movieCSV = fs.createWriteStream(
     encoding: 'utf8'
   }
 );
+
+function getRandomInt(max, min) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const generator = () => {
   (async () => {
@@ -28,7 +30,7 @@ const generator = () => {
       const actor = faker.name.findName();
       const character = faker.name.findName();
       const image_url = faker.image.imageUrl();
-      const role = faker.random.number();
+      const role = getRandomInt(3, 1);
       const movieToWrite = `${movie_id},${cast_id},${actor},${character},${image_url},${role}`;
       if (!movieCSV.write(movieToWrite + '\n', { flag: 'r+' })) {
         await new Promise(resolve => movieCSV.once('drain', resolve));
@@ -36,26 +38,5 @@ const generator = () => {
     }
   })();
 };
-
-// const generator = () => {
-//   csv.pipe(fs.createWriteStream('generated.csv'));
-//   for (var i = 0; i < generated; i++) {
-//     // if (i % 23 === 0) {
-//     //   movieIdCounter++;
-//     //   castIdCounter = -1;
-//     // }
-//     // castIdCounter++;
-//     csv.write({
-//       movie_id: movieIdCounter,
-//       cast_id: castIdCounter,
-//       actor: faker.name.findName(),
-//       character: faker.name.findName(),
-//       image_url: faker.image.imageUrl(),
-//       role: faker.random.number()
-//     });
-//   }
-//   csv.end();
-//   console.log('success!');
-// };
 
 generator();
